56ec7a6a5268ecad942660657d8dc6abb2448df0
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-12-31 16:56:46 +0530

    game in rust

--- Guessing_game/src/main.rs
+++ Guessing_game/src/main.rs
@@ -3,6 +3,9 @@ fn main(){
     println!("guess the number");
     println!("input the number guessed");
     let mut guess=String::new();
+    let apples = 5; // immutable
+    let mut bananas = 5; // 
+    println!("apple: {apples}");
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to read line");
