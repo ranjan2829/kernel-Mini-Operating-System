7a85afcf2dc3e27613c4b176fec32e14a2591509
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-01 07:05:20 +0530

    ownership

--- Concepts/src/main.rs
+++ Concepts/src/main.rs
@@ -115,12 +115,22 @@
 //     }
 //     println!("End count = {count}");
 // }
-fn main(){
-    let mut number=3;
-    while number!=0{
-        println!("{number}!");
-        number-=1;
-    }
-    println!("Liftoff!!");
+// fn main(){
+//     let mut number=3;
+//     while number!=0{
+//         println!("{number}!");
+//         number-=1;
+//     }
+//     println!("Liftoff!!");
 
+// }
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
 }
---
+++ OwnerShip/target/debug/deps/OwnerShip-e44c84f10e9da5bf.d
@@ -0,0 +1,5 @@
+/Users/ranjanshahajishitole/Desktop/Github/Rust/OwnerShip/target/debug/deps/OwnerShip-e44c84f10e9da5bf: src/main.rs
+
+/Users/ranjanshahajishitole/Desktop/Github/Rust/OwnerShip/target/debug/deps/OwnerShip-e44c84f10e9da5bf.d: src/main.rs
+
+src/main.rs:
