b45ff8545c3f0d68ab315e241aa55313a9b31839
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-01 05:52:15 +0530

    variables code

--- Concepts/src/main.rs
+++ Concepts/src/main.rs
@@ -27,3 +27,12 @@
 //     let tup: (i32, f64, u8) = (500, 6.4, 1);
 //     println!("{},{},{}", tup.0, tup.1, tup.2);
 // }
+fn main() {
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+}
