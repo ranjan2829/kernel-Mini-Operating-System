4e4352c8fcf6ffc0a75bf551ae5bcf9e9355685c
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-03 16:17:56 +0530

    rust ownership

--- OwnerShip/src/main.rs
+++ OwnerShip/src/main.rs
@@ -75,3 +75,14 @@
 //     s.push_str(",shitole");
 
 // }
+fn main() {
+    let mut s = String::from("hello");
+
+    {
+        let r1 = &mut s;
+        println!("{}",r1);
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
+    println!("{}",r2);
+}
