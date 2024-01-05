a360255a4c164646e0d36a68c3352c04d809621c
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-05 13:10:04 +0530

    rust code

--- OwnerShip/src/main.rs
+++ OwnerShip/src/main.rs
@@ -115,3 +115,14 @@
 
 //     s
 // }
+fn first_word(s:&String)->usize{
+    let bytes=s.as_bytes();
+    for(i,&item) in bytes.iter().enumerate(){
+        if item ==b' '{
+            return i;
+        }
+    }
+    s.len()
+}
+fn main(){}
+
