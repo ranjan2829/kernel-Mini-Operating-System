8b60b95054a6276152fd0605769524287e49000a
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-06 05:09:05 +0530

    struct data structure code

--- struct_data_structure/src/main.rs
+++ struct_data_structure/src/main.rs
@@ -18,3 +18,27 @@
 //     println!("{},{},{},{}",user1.active,user1.username,user1.mail,user1.sign_in);
 
 // }
+struct User {
+    active: bool,
+    Email: String,
+    name: String,
+    sign_in: u32,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        Email: email,
+        name: username,
+        sign_in: 1,
+    }
+}
+
+fn main() {
+    let mail = String::from("ranjan@");
+    let uname = String::from("ranjan3129");
+    //let var = build_user(email: mail, username: uname);
+    let var = build_user(mail, uname);
+
+    println!("{},{},{},{}", var.Email, var.sign_in, var.name, var.active);
+}
