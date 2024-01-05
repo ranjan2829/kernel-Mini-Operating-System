3f6cf2a1fa5f343675d2b884d86358c12710e58a
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-05 13:35:25 +0530

    rust code

--- OwnerShip/src/main.rs
+++ OwnerShip/src/main.rs
@@ -115,19 +115,20 @@
 
 //     s
 // }
-fn first_word(s:&String)->usize{
-    let bytes=s.as_bytes();
-    for(i,&item) in bytes.iter().enumerate(){
-        if item ==b' '{
-            return i;
-        }
-    }
-    s.len()
-}
-fn main(){
-    let word=String::from("ranjan");
-    let length = first_word(&word);
-    println!("{}",length);
+// fn first_word(s:&String)->usize{
+//     let bytes=s.as_bytes();
+//     for(i,&item) in bytes.iter().enumerate(){
+//         if item ==b' '{
+//             return i;
+//         }
+//     }
+//     s.len()
+// }
+// fn main(){
+//     let word=String::from("ran is the worst");
+//     let length = first_word(&word);
+//     println!("{}",length);
 
-}
+// }
+
 
