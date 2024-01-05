23e502b03c0a0aa9877038a558047fef6466ac49
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-06 05:02:08 +0530

    struct data structure code

--- struct_data_structure/src/main.rs
+++ struct_data_structure/src/main.rs
@@ -1,20 +1,20 @@
-struct user{
-    active:bool,
-    username:String,
-    mail:String,
-    sign_in:u64,
+// struct user{
+//     active:bool,
+//     username:String,
+//     mail:String,
+//     sign_in:u64,
 
-}
+// }
 
 
-fn main() {
-    let user1=user{
-        active:true,
-        username:String::from("ranjan3129"),
-        mail:String::from("@gmail.com"),
-        sign_in:24,
+// fn main() {
+//     let user1=user{
+//         active:true,
+//         username:String::from("ranjan3129"),
+//         mail:String::from("@gmail.com"),
+//         sign_in:24,
 
-    };
-    println!("{},{},{},{}",user1.active,user1.username,user1.mail,user1.sign_in);
+//     };
+//     println!("{},{},{},{}",user1.active,user1.username,user1.mail,user1.sign_in);
 
-}
+// }
