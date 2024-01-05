5c437863a7dfcd001392e25c7c1d6e3226bd7342
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-05 18:59:50 +0530

    rust code

--- OwnerShip/src/main.rs
+++ OwnerShip/src/main.rs
@@ -136,6 +136,19 @@
 //     let worst=&s[7..9];
 //     println!("{}{}",hello,worst);
 // }
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {}
 
 
 
