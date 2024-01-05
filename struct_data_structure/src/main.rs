dc78ad70e7b8eb5a2c64372e658fb07cf871853c
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-06 05:09:47 +0530

    struct data structure code

--- struct_data_structure/src/main.rs
+++ struct_data_structure/src/main.rs
@@ -1,44 +1,44 @@
-// struct user{
-//     active:bool,
-//     username:String,
-//     mail:String,
-//     sign_in:u64,
+// // struct user{
+// //     active:bool,
+// //     username:String,
+// //     mail:String,
+// //     sign_in:u64,
 
+// // }
+
+
+// // fn main() {
+// //     let user1=user{
+// //         active:true,
+// //         username:String::from("ranjan3129"),
+// //         mail:String::from("@gmail.com"),
+// //         sign_in:24,
+
+// //     };
+// //     println!("{},{},{},{}",user1.active,user1.username,user1.mail,user1.sign_in);
+
+// // }
+// struct User {
+//     active: bool,
+//     Email: String,
+//     name: String,
+//     sign_in: u32,
 // }
 
+// fn build_user(email: String, username: String) -> User {
+//     User {
+//         active: true,
+//         Email: email,
+//         name: username,
+//         sign_in: 1,
+//     }
+// }
 
 // fn main() {
-//     let user1=user{
-//         active:true,
-//         username:String::from("ranjan3129"),
-//         mail:String::from("@gmail.com"),
-//         sign_in:24,
+//     let mail = String::from("ranjan@");
+//     let uname = String::from("ranjan3129");
+    
+//     let var = build_user(mail, uname);
 
-//     };
-//     println!("{},{},{},{}",user1.active,user1.username,user1.mail,user1.sign_in);
-
+//     println!("{},{},{},{}", var.Email, var.sign_in, var.name, var.active);
 // }
-struct User {
-    active: bool,
-    Email: String,
-    name: String,
-    sign_in: u32,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        Email: email,
-        name: username,
-        sign_in: 1,
-    }
-}
-
-fn main() {
-    let mail = String::from("ranjan@");
-    let uname = String::from("ranjan3129");
-    //let var = build_user(email: mail, username: uname);
-    let var = build_user(mail, uname);
-
-    println!("{},{},{},{}", var.Email, var.sign_in, var.name, var.active);
-}
