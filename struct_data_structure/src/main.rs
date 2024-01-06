6bd5e1f61aa283890303def887cbd7d51fcb6273
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-06 12:02:28 +0530

    struct data structure code

--- struct_data_structure/src/main.rs
+++ struct_data_structure/src/main.rs
@@ -42,24 +42,49 @@
 
 //     println!("{},{},{},{}", var.Email, var.sign_in, var.name, var.active);
 // }
-struct user{
-    active :bool,
-    username:String,
-    email:String,
-    sign_in:u64,
+// struct user{
+//     active :bool,
+//     username:String,
+//     email:String,
+//     sign_in:u64,
+// }
+// fn build_user(email:String,username:String)->user{
+//     user{
+//         active:true,
+//         username:username,
+//         email:email,
+//         sign_in:1,
+//     }
+// }
+// fn main(){
+//     let user1=build_user(
+//         String::from("@@@@@@@"),
+//         String::from("ranjan3129"),
+//     );
+    
+// }
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
-fn build_user(email:String,username:String)->user{
-    user{
-        active:true,
-        username:username,
-        email:email,
-        sign_in:1,
-    }
-}
-fn main(){
-    let user1=build_user(
-        String::from("@@@@@@@"),
-        String::from("ranjan3129"),
-    );
+
+fn main() {
+    // --snip--
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };
     println!("{},{},{},{}",user1.email,user1.username,user1.sign_in,user1.active);
 }
