0170a6813702919431ce80d0f7e3c66c72b17d39
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-06 12:12:52 +0530

    struct data structure code

--- struct_data_structure/src/main.rs
+++ struct_data_structure/src/main.rs
@@ -88,3 +88,25 @@
 //     };
 //     println!("{},{},{},{}",user2.email,user2.username,user2.sign_in_count,user2.active);
 // }
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    // --snip--
+
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+}
