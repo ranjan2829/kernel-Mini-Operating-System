4be553063aaa779145aecd07fa30c64e6b6713fc
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-06 13:04:20 +0530

    struct data structure code

--- struct_data_structure/src/main.rs
+++ struct_data_structure/src/main.rs
@@ -137,6 +137,23 @@
 // fn area(dimensions:(u32,u32))->u32{
 //     dimensions.0 * dimensions.1
 // }
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+//     println!("Area: {}", area(&rect1));
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -147,9 +164,6 @@ fn main() {
         width: 30,
         height: 50,
     };
-    println!("Area: {}", area(&rect1));
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("rect1 is {:?}", rect1);
 }
