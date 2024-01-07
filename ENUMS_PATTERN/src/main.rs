b5e61c3225c95b7a69d90f9ef4adbb0f9e91c112
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-07 19:52:23 +0530

    rust enums and pattern code

--- ENUMS_PATTERN/src/main.rs
+++ ENUMS_PATTERN/src/main.rs
@@ -1,18 +1,18 @@
-#[derive(Debug)]
-enum IPRKIND {
-    V4,
-    V6,
-}
+// #[derive(Debug)]
+// enum IPRKIND {
+//     V4,
+//     V6,
+// }
 
-fn main() {
-    let four = IPRKIND::V4;
-    let six = IPRKIND::V6;
+// fn main() {
+//     let four = IPRKIND::V4;
+//     let six = IPRKIND::V6;
 
-    route(IPRKIND::V4);
-    route(IPRKIND::V6);
+//     route(IPRKIND::V4);
+//     route(IPRKIND::V6);
 
-    // Corrected println! statement
-    println!("{:?}{:?}", four, six);
-}
+//     // Corrected println! statement
+//     println!("{:?}{:?}", four, six);
+// }
 
-fn route(ip: IPRKIND) {}
+// fn route(ip: IPRKIND) {}
