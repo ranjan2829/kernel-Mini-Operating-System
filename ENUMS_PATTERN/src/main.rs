a061717314e997983b0a5571ecf91f3b7f235d3a
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-07 18:21:11 +0530

    rust enums and pattern code

--- ENUMS_PATTERN/src/main.rs
+++ ENUMS_PATTERN/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+enum IPRKIND{
+    V4,
+    V6,
 }
+fn main(){
+    let four=IPRKIND::V4;
+    let six=IPRKIND::V6;
+    route(IPRKIND::V4);
+    route(IPRKIND::V6);
+
+}
+fn route(ip:IPRKIND){}
