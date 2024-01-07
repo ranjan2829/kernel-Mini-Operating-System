bfd6e80c1ffb66af83bbb99a580c736c75905ce7
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-01-07 15:09:17 +0530

    rust method

--- struct_data_structure/src/main.rs
+++ struct_data_structure/src/main.rs
@@ -201,11 +201,17 @@
 //         println!("The rectangle has a nonzero width; it is {}", rect1.width);
 //     }
 // }
-#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
