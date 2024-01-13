f8a957cabcbaf7ca4f9707c486612cb360c55dd4
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2024-01-13 05:22:05 +0000

    match rusr code

--- ENUMS_PATTERN/src/main.rs
+++ ENUMS_PATTERN/src/main.rs
@@ -1,43 +1,43 @@
-#[derive(Debug)]
-// enum IPRKIND {
-//     V4,
-//     V6,
-// }
+// #[derive(Debug)]
+// // enum IPRKIND {
+// //     V4,
+// //     V6,
+// // }
 
-// fn main() {
-//     let four = IPRKIND::V4;
-//     let six = IPRKIND::V6;
+// // fn main() {
+// //     let four = IPRKIND::V4;
+// //     let six = IPRKIND::V6;
 
-//     route(IPRKIND::V4);
-//     route(IPRKIND::V6);
+// //     route(IPRKIND::V4);
+// //     route(IPRKIND::V6);
 
-//     // Corrected println! statement
-//     println!("{:?}{:?}", four, six);
-// }
+// //     // Corrected println! statement
+// //     println!("{:?}{:?}", four, six);
+// // }
 
-// fn route(ip: IPRKIND) {}
+// // fn route(ip: IPRKIND) {}
 
-enum IPRKIND {
-    V4,
-    V6,
-}
+// // enum IPRKIND {
+// //     V4,
+// //     V6,
+// // }
 
-struct IPV {
-    kind: IPRKIND,
-    address: String,
-}
+// // struct IPV {
+// //     kind: IPRKIND,
+// //     address: String,
+// // }
 
-fn main() {
-    let home = IPV {
-        kind: IPRKIND::V4,
-        address: String::from("137.0.0.1"),
-    };
+// // fn main() {
+// //     let home = IPV {
+// //         kind: IPRKIND::V4,
+// //         address: String::from("137.0.0.1"),
+// //     };
 
-    let homie = IPV {
-        kind: IPRKIND::V6,
-        address: String::from("192.168.0.1"),
-    };
+// //     let homie = IPV {
+// //         kind: IPRKIND::V6,
+// //         address: String::from("192.168.0.1"),
+// //     };
 
-    println!("{:?}, {:?}, {:?}, {:?}", home.kind, home.address, homie.kind, homie.address);
-}
+// //     println!("{:?}, {:?}, {:?}, {:?}", home.kind, home.address, homie.kind, homie.address);
+// // }
 
