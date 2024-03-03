d86627638cb4a3c9f88c05e4202fc407479c10b4
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-03 23:09:43 +0530

    generix rust

--- GenericTypes/src/main.rs
+++ GenericTypes/src/main.rs
@@ -1,23 +1,38 @@
-struct Point<T> {
-    x: T,
-    y: T,
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
 }
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-    fn y(&self) -> &T {
-        &self.y
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
 fn main() {
-    let p = Point { x: 5, y: 10 };
-    println!(" p.x ,p.y ====>>>>>>>>{},{}", p.x, p.y);
-} // struct Point<T> {
-  //     x: T,
-  //     y: T,
-  // }
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+//     fn y(&self) -> &T {
+//         &self.y
+//     }
+// }
 
 // fn main() {
 //     let integer = Point { x: 5, y: 10 };
