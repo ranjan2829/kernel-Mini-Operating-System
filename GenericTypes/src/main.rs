870f29a6e00fef8197554b1f20e52848b38a80d1
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-03 17:00:01 +0530

    generix rust

--- GenericTypes/src/main.rs
+++ GenericTypes/src/main.rs
@@ -1,25 +1,43 @@
+fn Largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-
-    let mut largest = &number_list[0];
-
-    for number in &number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("The largest number is {}", largest);
-
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let mut largest = &number_list[0];
-
-    for number in &number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("The largest number is {}", largest);
+    let result = Largest(&number_list);
+    println!("largest is {}", result);
+    let num_list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 99];
+    let ans = Largest(&num_list);
+    println!("largest of 2nd list ::::====>{} ", ans);
 }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let mut largest = &number_list[0];
+
+//     for number in &number_list {
+//         if number > largest {
+//             largest = number;
+//         }
+//     }
+
+//     println!("The largest number is {}", largest);
+
+//     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+//     let mut largest = &number_list[0];
+
+//     for number in &number_list {
+//         if number > largest {
+//             largest = number;
+//         }
+//     }
+
+//     println!("The largest number is {}", largest);
+// }
