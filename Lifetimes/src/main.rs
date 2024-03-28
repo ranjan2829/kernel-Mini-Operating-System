4d89c63aede5fddbb19b1e96665fa257808f9ca4
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-29 01:23:48 +0530

    lifetimes

--- Lifetimes/src/main.rs
+++ Lifetimes/src/main.rs
@@ -26,13 +26,14 @@
 //         println!("The longest string is {}", result);
 //     }
 // }
-struct Important<'a>{
+struct ImportantExcerpt<'a> {
     part: &'a str,
 }
-fn main(){
-    let novel=String::from("Call me Ranjan , Some years ago ....");
-    let first=novel.split(".").next().except("could not find a '.'");
-    let i Important{
-        part:first,
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
     };
 }
