bb36360cffb04827104a7f9f3916e89a28b0b8db
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-29 00:13:39 +0530

    lifetimes

--- Lifetimes/src/main.rs
+++ Lifetimes/src/main.rs
@@ -1,8 +1,26 @@
+// fn main() {
+//     let r;
+
+//     let x = 10;
+//     r = &x;
+
+//     println!("{}", r);
+// }
+//
+
+// Assuming longest function is defined in another module called utils
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
 fn main() {
-    let r;
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    let x = 10;
-    r = &x;
-
-    println!("{}", r);
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
 }
