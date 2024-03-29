ee33e1ac21697434d48a6813a8477c5b5ea8a3fd
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-29 17:47:49 +0530

    tests in rust

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -119,8 +119,31 @@
 //         assert!(!smaller.can_hold(&larger));
 //     }
 // }
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+// pub fn add_two(a: i32) -> i32 {
+//     a + 2
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_adds_two() {
+//         assert_eq!(4, add_two(2));
+//     }
+// }
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
@@ -128,7 +151,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
