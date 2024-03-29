ada0ef8fff32fb498fe54198175e9cd16f4b6f25
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-29 13:21:23 +0530

    tests in rust

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -16,3 +16,61 @@
 //         panic!("Make this Test Fail ");
 //     }
 // }
+// #[derive(Debug)]
+// struct tect {
+//     width: u32,
+//     height: u32,
+// }
+// impl rect {
+//     fn hold(&self, other: &rect) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     #[test]
+//     fn larger() {
+//         let large = rect {
+//             width: 8,
+//             height: 9,
+//         };
+//         let small = rect {
+//             width: 3,
+//             height: 4,
+//         };
+//         assert!(large.hold(&small));
+//     }
+// }
+//
+//
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+}
