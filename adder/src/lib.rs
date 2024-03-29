85ba9a32fe14035d02d6e98cd79b8b31338ae641
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-29 17:51:23 +0530

    tests in rust

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -132,14 +132,48 @@
 //         assert_eq!(4, add_two(2));
 //     }
 // }
+// pub struct Guess {
+//     value: i32,
+// }
+
+// impl Guess {
+//     pub fn new(value: i32) -> Guess {
+//         if value < 1 || value > 100 {
+//             panic!("Guess value must be between 1 and 100, got {}.", value);
+//         }
+
+//         Guess { value }
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     #[should_panic]
+//     fn greater_than_100() {
+//         Guess::new(200);
+//     }
+// }
 pub struct Guess {
     value: i32,
 }
 
+// --snip--
+
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
         }
 
         Guess { value }
@@ -151,7 +185,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
