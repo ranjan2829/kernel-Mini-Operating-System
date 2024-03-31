3f4519ef8fc500d714ba47109da55e5fb72a4ccc
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-03-31 14:04:54 +0530

    tests rust

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,196 +1,195 @@
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn exploration() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-//     #[test]
-//     fn another() {
-//         panic!("Make this Test Fail ");
-//     }
-// }
-// #[derive(Debug)]
-// struct tect {
-//     width: u32,
-//     height: u32,
-// }
-// impl rect {
-//     fn hold(&self, other: &rect) -> bool {
-//         self.width > other.width && self.height > other.height
-//     }
-// }
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     #[test]
-//     fn larger() {
-//         let large = rect {
-//             width: 8,
-//             height: 9,
-//         };
-//         let small = rect {
-//             width: 3,
-//             height: 4,
-//         };
-//         assert!(large.hold(&small));
-//     }
-// }
-//
-//
-// #[derive(Debug)]
-// struct Rectangle {
-//     width: u32,
-//     height: u32,
-// }
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+    #[test]
+    fn another() {
+        panic!("Make this Test Fail ");
+    }
+}
+#[derive(Debug)]
+struct tect {
+    width: u32,
+    height: u32,
+}
+impl rect {
+    fn hold(&self, other: &rect) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn larger() {
+        let large = rect {
+            width: 8,
+            height: 9,
+        };
+        let small = rect {
+            width: 3,
+            height: 4,
+        };
+        assert!(large.hold(&small));
+    }
+}
 
-// impl Rectangle {
-//     fn can_hold(&self, other: &Rectangle) -> bool {
-//         self.width > other.width && self.height > other.height
-//     }
-// }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 
-//     #[test]
-//     fn larger_can_hold_smaller() {
-//         let larger = Rectangle {
-//             width: 8,
-//             height: 7,
-//         };
-//         let smaller = Rectangle {
-//             width: 5,
-//             height: 1,
-//         };
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//         assert!(larger.can_hold(&smaller));
-//     }
-//}
-// #[derive(Debug)]
-// struct Rectangle {
-//     width: u32,
-//     height: u32,
-// }
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
 
-// impl Rectangle {
-//     fn can_hold(&self, other: &Rectangle) -> bool {
-//         self.width > other.width && self.height > other.height
-//     }
-//}
+        assert!(larger.can_hold(&smaller));
+    }
+}
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 
-//     #[test]
-//     fn larger_can_hold_smaller() {
-//         // --snip--
-//         let larger = Rectangle {
-//         width: 8,
-//         height: 7,
-//     };
-//     let smaller = Rectangle {
-//         width: 5,
-//         height: 1,
-//     };
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     assert!(larger.can_hold(&smaller));
-// }
+    #[test]
+    fn larger_can_hold_smaller() {
+        // --snip--
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
 
-// #[test]
-// fn smaller_cannot_hold_larger() {
-//     let larger = Rectangle {
-//         width: 8,
-//         height: 7,
-//     };
-//         let smaller = Rectangle {
-//             width: 5,
-//             height: 1,
-//         };
+        assert!(larger.can_hold(&smaller));
+    }
 
-//         assert!(!smaller.can_hold(&larger));
-//     }
-// }
-// pub fn add_two(a: i32) -> i32 {
-//     a + 2
-// }
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+        assert!(!smaller.can_hold(&larger));
+    }
+}
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
 
-//     #[test]
-//     fn it_adds_two() {
-//         assert_eq!(4, add_two(2));
-//     }
-// }
-// pub struct Guess {
-//     value: i32,
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// impl Guess {
-//     pub fn new(value: i32) -> Guess {
-//         if value < 1 || value > 100 {
-//             panic!("Guess value must be between 1 and 100, got {}.", value);
-//         }
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+}
+pub struct Guess {
+    value: i32,
+}
 
-//         Guess { value }
-//     }
-// }
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+        Guess { value }
+    }
+}
 
-//     #[test]
-//     #[should_panic]
-//     fn greater_than_100() {
-//         Guess::new(200);
-//     }
-// }
-// pub struct Guess {
-//     value: i32,
-// }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-// // --snip--
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+pub struct Guess {
+    value: i32,
+}
 
-// impl Guess {
-//     pub fn new(value: i32) -> Guess {
-//         if value < 1 {
-//             panic!(
-//                 "Guess value must be greater than or equal to 1, got {}.",
-//                 value
-//             );
-//         } else if value > 100 {
-//             panic!(
-//                 "Guess value must be less than or equal to 100, got {}.",
-//                 value
-//             );
-//         }
+// --snip--
 
-//         Guess { value }
-//     }
-// }
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+        Guess { value }
+    }
+}
 
-//     #[test]
-//     #[should_panic(expected = "less than or equal to 100")]
-//     fn greater_than_100() {
-//         Guess::new(200);
-//     }
-// }
-//
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ adder/target/debug/deps/adder-54544a52dfd16a0e.d
@@ -0,0 +1,5 @@
+/Users/ranjanshahajishitole/Desktop/Github/Rust/adder/target/debug/deps/libadder-54544a52dfd16a0e.rmeta: src/lib.rs
+
+/Users/ranjanshahajishitole/Desktop/Github/Rust/adder/target/debug/deps/adder-54544a52dfd16a0e.d: src/lib.rs
+
+src/lib.rs:
