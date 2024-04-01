2bcc5542f518d8af9769755f2eb1bc1bf01493c6
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-04-02 00:49:48 +0530

    tests rust

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -190,14 +190,38 @@
 //     }
 // }
 
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() -> Result<(), String> {
+//         if 2 + 2 == 4 {
+//             Ok(())
+//         } else {
+//             Err(String::from("two plus two does not equal four"))
+//         }
+//     }
+// }
+//
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
     }
 }
