5d6784797617ebffb5fe222ca86654339492a8f4
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-04-03 11:53:06 +0530

    minigrep Rust CLI

--- minigrp/src/main.rs
+++ minigrp/src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    //iterator on the command line to collect the string
+    dbg!(args);
 }
