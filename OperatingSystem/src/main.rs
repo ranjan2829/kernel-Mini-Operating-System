385f0c1ce2fc1e7af57f881302c5ae75757f4f5d
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-01 17:16:28 +0530

    done

--- OperatingSystem/src/main.rs
+++ OperatingSystem/src/main.rs
@@ -9,9 +9,10 @@ static HELLO: &[u8] = b"Hello Sakshi, welcome to Ranjan's Kernel OS!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut writer = vga_buffer::WRITER.lock();
-    writer.write_str("HELLO AGAIN WORLD < WELCOME ").unwrap();
-    write!(writer, ", some numbers: {} {}", 42, 1.337).unwrap();
+    //let mut writer = vga_buffer::WRITER.lock();
+    //writer.write_str("HELLO AGAIN WORLD < WELCOME ").unwrap();
+    //write!(writer, ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
