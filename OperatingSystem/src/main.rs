148ab9cd1a03583dc7d732ecff4484381f8f3021
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-06-01 13:12:10 +0530

    done

--- OperatingSystem/src/main.rs
+++ OperatingSystem/src/main.rs
@@ -7,14 +7,15 @@ static HELLO: &[u8] = b"Hello Sakshi , welcome to Ranjan's Kernel OS !";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    //let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    vga_buffer::print_something();
 
     loop {}
 }
